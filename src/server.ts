3e226c4b0528356a8ad44d933eab1216261fee44
md-alamin2 <mdalamin22671@gmail.com>
2025-12-03 23:43:24 +0600

    create user and todo table in neon db with express

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,53 @@
 import express, { Request, Response } from 'express';
-import { Pool } from 'pg'
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({path: path.join(process.cwd(), ".env")})
 
 const app = express();
 const port = 5000;
-const pool = new Pool({
-    connectionString:
-        `postgresql://neondb_owner:npg_GVLbyIj8B4DM@ep-shy-lab-ahylhfl2-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
-})
 
 // middle ware
 app.use(express.json())
+
+
+// DB
+const pool = new Pool({
+    connectionString:
+        `${process.env.CONNECTION_STR}`
+})
+
+const initDB=async ()=>{
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(110) UNIQUE NOT NULL,
+        age INT,
+        address TEXT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `)
+
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS todos(
+        id SERIAL PRIMARY KEY,
+        user_id INT REFERENCES users(id) ON DELETE CASCADE,
+        title VARCHAR(200) NOT NULL,
+        description TEXT,
+        completed BOOLEAN DEFAULT false,
+        due_date DATE,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `)
+}
+
+initDB();
+
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('hello next level development')
