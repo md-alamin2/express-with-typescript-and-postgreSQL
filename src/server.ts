5d4e76853449629cf59a8aba667cf50dbee26606
md-alamin2 <mdalamin22671@gmail.com>
2025-12-03 22:40:17 +0600

    learn how create a post api in ts with express and how to create posgreSQL pool

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,29 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { Pool } from 'pg'
+
 const app = express();
 const port = 5000;
+const pool = new Pool({
+    connectionString:
+        `postgresql://neondb_owner:npg_GVLbyIj8B4DM@ep-shy-lab-ahylhfl2-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
+})
 
-app.get('/',(req, res)=>{
+// middle ware
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
     res.send('hello next level development')
 })
 
-app.listen(port, ()=>{
+app.post('/', (req: Request, res: Response) => {
+    console.log(req.body);
+
+    res.status(201).send({
+        success: true,
+        message: 'Api is working'
+    })
+})
+
+app.listen(port, () => {
     console.log(`server is running on port:${port}`)
 })
