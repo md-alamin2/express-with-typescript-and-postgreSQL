b708eb1fbf8baff86850d48fd780b3a6b0bcdfbf
md-alamin2 <mdalamin22671@gmail.com>
2025-12-05 00:19:20 +0600

    create a post api to post todos

--- src/server.ts
+++ src/server.ts
@@ -173,6 +173,27 @@ app.delete('/users/:id', async (req: Request, res: Response) => {
     }
 })
 
+// todos crud
+app.post('/todos', async(req: Request, res: Response)=>{
+    const {user_id, title}= req.body;
+
+    try{
+        const result = await pool.query(`INSERT INTO todos(user_id, title) VALUES($1, $2) RETURNING *`, [user_id, title])
+
+        res.status(201).json({
+            success: true,
+            message: "Todo added successfully",
+            data: result.rows[0]
+        })
+
+    }catch(err: any){
+        res.status(500).json({
+            success: false,
+            message:err.message
+        })
+    }
+})
+
 app.listen(port, () => {
     console.log(`server is running on port:${port}`)
 })
