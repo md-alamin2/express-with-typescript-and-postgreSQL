f0e3ade7b61b63f8ef176d9b87a303a09ccb9d80
md-alamin2 <mdalamin22671@gmail.com>
2025-12-07 22:29:08 +0600

    create a new user table with password

--- src/config/db.ts
+++ src/config/db.ts
@@ -13,6 +13,7 @@ const initDB = async () => {
         id SERIAL PRIMARY KEY,
         name VARCHAR(100) NOT NULL,
         email VARCHAR(110) UNIQUE NOT NULL,
+        password TEXT NOT NULL,
         age INT,
         address TEXT,
         created_at TIMESTAMP DEFAULT NOW(),
